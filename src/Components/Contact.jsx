d0bebac16c54d72e7d867240bc77d6d2c437b5f9
Eduardo Camacho <eduardocamacho4977@gmail.com>
2026-06-22 17:02:45 -0500

    Agrego botón para volver desde Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,15 +1,25 @@
-//import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 import imagencontacto from "../assets/contact.jpg"
+function Contact  ()  {
 
-const Contact = () => {
+  const navigate = useNavigate()
+
+  const volverNavbar = () => {
+    navigate('/')
+  }
+
   return (
     <div className='contact'>
 
-        <h2>Este es el componente Contact</h2>
-       
-       <img src={imagencontacto} alt='imagen contact'  width="600" />
-      
+      <h2>Este es el componente Contact</h2>
+
+      <img
+        src={imagencontacto}alt='imagen contact' width="600"/>
+
+      <br />
+
+      <button onClick={volverNavbar}> Regresar al Navbar </button>
+
     </div>
   )
 }
